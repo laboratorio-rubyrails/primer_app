3118d27e48cdbdd8acf694327d1c5a508189c834
Pavel <laboratorio.rubyrails@gmail.com>
2014-05-11 21:40:32 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrimerApp::Application.config.secret_key_base = 'a01ffbb96abea02724519ae2560ef796600c6699c9e22b1ff79f18861c29fc3180f7693c80c032e4cef6f1452996b05f9613a0d76325a8f9989b1381d60c11a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PrimerApp::Application.config.session_store :cookie_store, key: '_primer_app_session'
